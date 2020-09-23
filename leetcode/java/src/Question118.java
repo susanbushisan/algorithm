7687bb74bf5b56f517ec15fb6efd6a23daa9705a
susanbushisan <linkmao196@163.com>
2020-09-24 00:11:16 +0800

    杨辉三角

---
+++ leetcode/java/src/Question118.java
@@ -0,0 +1,33 @@
+
+
+import java.util.*;
+
+public class Question118 {
+
+
+    public static void main(String[] args) {
+        Question118 q = new Question118();
+        long start = System.currentTimeMillis();
+        System.out.println(q.generate(5));
+        System.out.println(System.currentTimeMillis() - start + "ms");
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            for (int j = 1; j <= i; j++) {
+                if (j == i){
+                    temp.add(1);
+                    break;
+                }
+                List<Integer> pre = ans.get(ans.size() - 1);
+                temp.add(pre.get(j - 1)+pre.get(j));
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+
+}
