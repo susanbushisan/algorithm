fd99808fffe44bdafc22dd89adccc6b8fed22dd0
susanbushisan <linkmao196@163.com>
2020-09-24 10:06:17 +0800

    模拟除法

---
+++ leetcode/java/src/Question166.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question166 {
+    public static void main(String[] args) {
+        Question166 question164 = new Question166();
+        long start = System.currentTimeMillis();
+        System.out.println(question164.fractionToDecimal(1,2));
+        System.out.println(System.currentTimeMillis() - start + "ms");
+    }
+
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder fraction = new StringBuilder();
+        if (numerator < 0 ^ denominator < 0) {
+            fraction.append("-");
+        }
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+        fraction.append(dividend / divisor);
+        long remainder = dividend % divisor;
+        if (remainder == 0) {
+            return fraction.toString();
+        }
+        fraction.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                fraction.insert(map.get(remainder), "(");
+                fraction.append(")");
+                break;
+            }
+            map.put(remainder, fraction.length());
+            remainder *= 10;
+            fraction.append(String.valueOf(remainder / divisor));
+            remainder %= divisor;
+        }
+        return fraction.toString();
+    }
+
+}
