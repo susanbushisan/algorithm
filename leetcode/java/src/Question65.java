7d392129ae4a074c1c347648925c5547c4bb5253
susanbushisan <linkmao196@163.com>
2020-10-11 14:17:45 +0800

    有效数字

---
+++ leetcode/java/src/Question65.java
@@ -0,0 +1,53 @@
+
+
+public class Question65 {
+
+
+    public static void main(String[] args) {
+        Question65 q = new Question65();
+
+        long start = System.currentTimeMillis();
+        System.out.println(q.isNumber(".1"));
+        System.out.println(System.currentTimeMillis() - start + "ms");
+    }
+
+    public int make(char c) {
+        switch(c) {
+            case ' ': return 0;
+            case '+':
+            case '-': return 1;
+            case '.': return 3;
+            case 'e': return 4;
+            default:
+                if(c >= 48 && c <= 57) return 2;
+        }
+        return -1;
+    }
+
+    public boolean isNumber(String s) {
+        int state = 0;
+        int finals = 0b101101000;
+        int[][] transfer = new int[][]{{ 0, 1, 6, 2,-1},
+                {-1,-1, 6, 2,-1},
+                {-1,-1, 3,-1,-1},
+                { 8,-1, 3,-1, 4},
+                {-1, 7, 5,-1,-1},
+                { 8,-1, 5,-1,-1},
+                { 8,-1, 6, 3, 4},
+                {-1,-1, 5,-1,-1},
+                { 8,-1,-1,-1,-1}};
+        char[] ss = s.toCharArray();
+        for (char c : ss) {
+            int id = make(c);
+            if (id < 0){
+                return false;
+            }
+            state = transfer[state][id];
+            if (state < 0){
+                return false;
+            }
+        }
+        return (finals & (1 << state)) > 0;
+    }
+
+}
